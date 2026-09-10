10ba666b40c75f5a8c459a2ef800d13977a98473
HimanshuuKumar <hk3196316@gmail.com>
2026-09-09 20:57:49 -0400

    Add backend health check and update frontend

--- api/server.js
+++ api/server.js
@@ -19,6 +19,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/video", videoRoute);
+app.get("/", (req, res) => {
+  return res.json({ message: "request access success" });
+});
 const port = 4000;
 
 app.listen(port, () => console.log(`server is rounning on port ${port}`));
